b24aed37ec22519f0a5225f197939459dac80db6
jasburns42 <157663308+jasburns42@users.noreply.github.com>
2024-02-06 12:58:06 -0500

    Cleaned up. Added docstrings. Retry if one failure

--- src/Scraper.py
+++ src/Scraper.py
@@ -7,14 +7,16 @@ class Scraper:
     data from websites
     """
     def __init__(self):
-        pass
+        print("Should not be able to call the parent __init__")
     def __repr__(self):
         return "This is the Scraper class"
     def check_site(self, url):
+        """Make sure we connect to site correctly"""
         self.url = url
         self.html_data = requests.get(self.url)
         return self.html_data
     def get_html_data(self):
+        """Pull the html data from the site and use BS to do initial parsing"""
         soup = BeautifulSoup(self.html_data.content, "html.parser")
         return soup
 
@@ -22,9 +24,16 @@ class Goodreads_Scraper(Scraper):
     """
     This scraper parses results from Goodreads
     """
+    def __init__(self):
+        print("Created a Goodreads Scraper")
+    def __repr__(self):
+        return "This is the Goodreads Scraper class"
     def parse(self, soup):
+        """
+        This parser is customized for Goodreads. Takes the html
+        data gathered from the page and pulls out book data.
+        """
         results = soup.find(id="booksBody")
-        # print(results)
         book_titles = results.find_all("td", class_="field title")
         titles = []
         for book_title in book_titles:
@@ -46,10 +55,8 @@ if __name__ == "__main__":
     url = "https://www.goodreads.com/review/list/89246357-jason-burns?page=1&per_page=20&ref=nav_mybooks&utf8=%E2%9C%93"
     book_scraper = Goodreads_Scraper()
     response = book_scraper.check_site(url)
-    print(response)
     html_data = book_scraper.get_html_data()
     book_titles, book_authors = book_scraper.parse(html_data)
-    
     print(list(zip(book_titles, book_authors)))
     
         
--- src/main.py
+++ src/main.py
@@ -1,18 +1,26 @@
 from Scraper import Goodreads_Scraper
-from pickle import TRUE
+
 
 if __name__ == "__main__":
+    """
+    Run this script to gather books I have read from Goodreads.
+    """
     book_scraper = Goodreads_Scraper()
     page = 0
     book_data = []
-    while TRUE:
+    scraping_error = False
+    while True:
         page += 1
         url = f"https://www.goodreads.com/review/list/89246357-jason-burns?page={page}&per_page=20&ref=nav_mybooks&utf8=%E2%9C%93"
-        print(url)
         response = book_scraper.check_site(url)
-        if not response.ok:
-            print(f"Error reading page {page}. Stopping")
+        if not response.ok and not scraping_error:
+            print(f"Error reading page {page}. Try again")
+            scraping_error = True
+            continue
+        if not response.ok and scraping_error:
+            print("Two errors in a row. Something is wrong. Stopping")
             break
+        scraping_error = False
         print(f"Reading page {page}")
         html_data = book_scraper.get_html_data()
         book_titles, book_authors = book_scraper.parse(html_data)
@@ -21,8 +29,8 @@ if __name__ == "__main__":
             break
         for title, author in zip(book_titles, book_authors):
             book_data.append((title, author))
-        # print(list(zip(book_titles, book_authors)))
     print(book_data)
-    print(len(book_data))
+    print(f"I have read {len(book_data)} books!!!")
     # Gathered Titles and Authors into a list of tuples.
     # Add rest of data, then push to database
+    
